402f3a5a59e2828ff3fe3e36e9278293a7e95ec5
Jiawei-Kuang <jiawei.kuang0818@gmail.com>
2016-09-05 14:14:15 -0700

    First commit with cover picture and basic info.

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name yiwebsiteApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the yiwebsiteApp
+ */
+angular.module('yiwebsiteApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name yiwebsiteApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the yiwebsiteApp
+ */
+angular.module('yiwebsiteApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
